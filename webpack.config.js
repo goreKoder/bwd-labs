8e42e572d2e72fb397e024551782c92934974b48
Gleb Drozdov <glebdrozdov203@gmail.com>
2024-12-14 14:29:39 +0400

    feat: LAB4-3 Добавление ранее созданных страниц в проект

--- webpack.config.js
+++ webpack.config.js
@@ -37,18 +37,31 @@ module.exports = {
     plugins: [
 
         new HtmlWebpackPlugin({
-
+     
             template: './src/index.html',
-
+     
             inject: true,
-
+     
             chunks: ['index'],
-
+     
             filename: 'index.html'
-
+     
         }),
-
-    ],
+     
+        new HtmlWebpackPlugin({
+     
+            template: './src/secondPage.html',
+     
+            inject: true,
+     
+            chunks: ['index'],
+     
+            filename: 'secondPage.html'
+     
+     
+        }),
+     
+     ],
 
 
     devServer: {
